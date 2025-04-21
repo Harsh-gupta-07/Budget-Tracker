b9e5c8f5b9a3ba84a5b8db950b432e7a1ad60a5e
Harshvardhan Gupta <harshvardhangofficial@gmail.com>
2025-04-21 22:25:43 +0530

    lets do this

--- app/page.js
+++ app/page.js
@@ -1,72 +1,29 @@
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <div className="navbar bg-base-100 shadow-sm lg:hidden">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>{" "}
-            </svg>
-          </button>
-        </div>
+    <div className="h-screen flex flex-col">
+      {/* Navbar: Only visible on small/medium screens */}
+      <div className="block lg:hidden">
+        <Navbar />
       </div>
 
-      <div className="drawer lg:drawer-open">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-side">
-          <label
-            htmlFor="my-drawer-2"
-            aria-label="close sidebar"
-            className="drawer-overlay"
-          ></label>
-          <ul className="menu bg-base-200 min-h-full w-65 py-4 px-0 text-gray-300">
-            {/* Sidebar content here */}
-            <li className="pointer-events-none border-b  border-gray-400 p-[16px] pt-0 w">
-              <h1 className="text-xl m-auto text-center font-bold">Budget Tracker</h1>
-            </li>
-            <li className="mt-5">
-              <div className="flex items-center justify-start py-3 rounded-none pl-6">
-                <Image  src="/block_15713564.png" width={25} height={25} alt="dashboard-logo"/>
-                <p className="text-base font-medium ml-1`````````">Dashboard</p>
-              </div>
-            </li>
-            <li>
-              <div className="flex items-center justify-start py-3 rounded-none pl-6">
-                <Image  src="/wallet-4-svgrepo-com (2).svg" width={25} height={25} alt="dashboard-logo"/>
-                <p className="text-base font-medium ml-1">Budget Categories</p>
-              </div>
-            </li>
-            <li>
-              <div className="flex  items-center justify-start py-3 rounded-none pl-6">
-                <Image  src="/bill-svgrepo-com (1).svg" width={25} height={25} alt="dashboard-logo"/>
-                <p className="text-base font-medium ml-1">Expenses</p>
-              </div>
-            </li>
-            <li>
-              <div className="flex items-center justify-start py-3 rounded-none pl-6">
-                <Image  src="/bell-svgrepo-com.svg" width={25} height={25} alt="dashboard-logo"/>
-                <p className="text-base font-medium ml-1">Reminders</p>
-              </div>
-            </li>
-          </ul>
+      <div className="flex flex-1">
+        {/* Sidebar: Only visible on large screens and up */}
+        <div className="hidden lg:block w-65 bg-base-200">
+          <Sidebar />
+        </div>
+
+        {/* Main content */}
+        <div className="w-full h-full">
+          <div className="flex flex-row bg-base-100 p-6  justify-between align-middle">
+            <h1 className="text-2xl font-bold inline align-middle mt-[5px]">Dashboard</h1>
+            <button className="bg-base-200 hover:bg-base-300 rounded-full btn w-10 h-10 p-3 border-none"><Image src="/plus-svgrepo-com.svg" alt="plus img" width={20} height={20} /></button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
